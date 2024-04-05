07480c60f788490be6b3e24db7a1f79714edf7dd
Sumuel-G <samuellarsgilbe@gmail.com>
2024-04-05 13:39:26 +0200

    reeling fix and reel sign

--- Fishing game/Assets/Scripts/Bobber.cs
+++ Fishing game/Assets/Scripts/Bobber.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class Bobber : MonoBehaviour
 {
     [SerializeField] Rigidbody2D bobber;
+    [SerializeField] GameObject Player;
     public bool inwater = false;
+    private void Update()
+    {
+        gameObject.transform.eulerAngles = Vector3.zero;
+        if (inwater) { bobber.gravityScale = 0.25f; bobber.mass = 0.25f; }
+        else { bobber.mass = 1; }
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Water")) { bobber.gravityScale = 0.25f; }
+        if (collision.CompareTag("Water")) { inwater = true; }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water")) { inwater = false; bobber.gravityScale = 1; }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Wall")) { gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero; }
+        if (collision.collider.CompareTag("Wall")) 
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0;
+        }
     }
 }
--- Fishing game/Assets/Scripts/Player.cs
+++ Fishing game/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -10,27 +11,78 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject fishingrod;
     [SerializeField] GameObject bobber;
     [SerializeField] GameObject bobberstart;
+    [SerializeField] GameObject MainCamera;
+    [SerializeField] GameObject BobberCamera;
+    [SerializeField] GameObject reelSign;
+    [SerializeField] bool enableextracam;
     public List<string> Caughtfish = new List<string>();
     public float chargetime;
+    bool reel;
     bool charging;
     bool hascast;
     float allowedtime = 0;
+    float reelsigntimer;
+    float reeltimer;
+    System.Random rng;
     // Start is called before the first frame update
     void Start()
     {
+        rng = new System.Random();
         while (Caughtfish.Count > 0) { Caughtfish.RemoveAt(0); }
         bobberstart.transform.position = bobber.transform.position;
     }
     private void Update()
     {
-        if (chargetime >= 3) chargetime = 3;
+        if (bobber.GetComponent<Bobber>().inwater)
+        {
+            reeltimer += Time.deltaTime;
+            if (reeltimer >= 2) { Fishing(); reeltimer = 0; }
+        }
+        if (reel && bobber.GetComponent<Bobber>().inwater)
+        {
+            if (reelSign.activeInHierarchy) 
+            {
+                reelsigntimer += Time.deltaTime;
+                if (reelsigntimer >= 2) { reelSign.SetActive(false); reelsigntimer = 0; }
+            }
+            else 
+            {
+                reelsigntimer += Time.deltaTime;
+                if (reelsigntimer >= 1) { reelSign.SetActive(true); reelsigntimer = 0; }
+            }
+            Vector2 targetPos = transform.position;
+            Vector3 dir = targetPos - (Vector2)bobber.transform.position;
+            bobber.transform.up = dir;
+            bobber.GetComponent<Rigidbody2D>().gravityScale = 0;
+            if (bobber.GetComponent<Bobber>().inwater) bobber.GetComponent<Rigidbody2D>().AddForce(-bobber.transform.up * 0.375f);
+        }
+        else if (reel && !bobber.GetComponent<Bobber>().inwater)
+        {
+            bobber.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            bobber.GetComponent<Rigidbody2D>().gravityScale = 0;
+            if (enableextracam)
+            {
+                MainCamera.SetActive(true);
+                BobberCamera.SetActive(false);
+            }
+            bobber.transform.position = bobberstart.transform.position;
+            reelSign.SetActive(false);
+            reel = false;
+        }
+        if (chargetime >= 3) { chargetime = 3; reel = false; }
         if (charging)
         {
+            reel = false;
             if (hascast) 
             {
                 bobber.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                bobber.GetComponent<Rigidbody2D>().gravityScale = 0;
+                if (enableextracam)
+                {
+                    MainCamera.SetActive(true);
+                    BobberCamera.SetActive(false);
+                }
             }
+            bobber.GetComponent<Rigidbody2D>().gravityScale = 0;
             hascast = false;
             bobber.transform.position = bobberstart.transform.position;
             chargetime += Time.deltaTime;
@@ -38,6 +90,7 @@ public class Player : MonoBehaviour
         }
         else if (chargetime >= .5f)
         {
+            reel = false;
             fishingrod.transform.eulerAngles = new Vector3(0, 0, 75);
             Cast();
             chargetime -= 0.05f;
@@ -60,14 +113,23 @@ public class Player : MonoBehaviour
     }
     public void ChargeandReel(InputAction.CallbackContext context)
     {
-        Debug.Log("charge");
-        if (context.performed)
+        if (reel)
         {
-            charging = true;
+            Vector2 targetPos = transform.position;
+            Vector3 dir = targetPos - (Vector2)bobber.transform.position;
+            bobber.transform.up = dir;
+            bobber.GetComponent<Rigidbody2D>().AddForce(bobber.transform.up * 28);
         }
         else
         {
-            charging = false;
+            if (context.performed)
+            {
+                charging = true;
+            }
+            else
+            {
+                charging = false;
+            }
         }
     }
     private void Cast()
@@ -77,6 +139,20 @@ public class Player : MonoBehaviour
         bobber.transform.up = dir;
         bobber.GetComponent<Rigidbody2D>().AddForce(bobber.transform.up * (chargetime * 20));
         bobber.GetComponent<Rigidbody2D>().gravityScale = 1;
+        if (enableextracam)
+        {
+            MainCamera.SetActive(false);
+            BobberCamera.SetActive(true);
+        }
         hascast = true;
+    }
+    private void Fishing()
+    {
+        int temp = rng.Next(0, 1000000);
+        if (temp > 10 && !reel)
+        {
+            reel = true;
+            reelSign.SetActive(true);
+        }
     }
 }
